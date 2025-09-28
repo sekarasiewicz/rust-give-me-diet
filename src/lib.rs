2196eaaf7ffe0d63dbf68c61f7570665d369e4ff
sekarasiewicz <sekarasiewicz@gmail.com>
2025-09-28 20:34:34 +0200

    Update dependencies in Cargo.toml and Cargo.lock for improved functionality
    This commit adds `console_error_panic_hook` and `wasm-bindgen` as dependencies in `Cargo.toml`, and updates the `windows-sys` dependency to version 0.61.1 across multiple entries in `Cargo.lock`. Additionally, the `hydrate` function in `lib.rs` is modified to include error handling with `console_error_panic_hook`, enhancing the application's resilience during runtime.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use leptos::mount::mount_to_body;
-use leptos::{view, wasm_bindgen};
+use wasm_bindgen::prelude::wasm_bindgen;
 
 pub mod app;
 
-#[wasm_bindgen::prelude::wasm_bindgen]
+#[wasm_bindgen]
 pub fn hydrate() {
-    mount_to_body(|| {
-        view! { <app::App/> }
-    });
+    console_error_panic_hook::set_once();
+    mount_to_body(app::App);
 }
